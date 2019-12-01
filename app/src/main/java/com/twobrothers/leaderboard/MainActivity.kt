c0e7d39bdf6d7a67d3fcc17069086b3cf94de5b3
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-12-01 11:43:39 +1100

    init firebase

--- app/src/main/java/com/twobrothers/leaderboard/MainActivity.kt
+++ app/src/main/java/com/twobrothers/leaderboard/MainActivity.kt
@@ -2,11 +2,17 @@ package com.twobrothers.leaderboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+        db.collection("sports").get().addOnSuccessListener {
+            println(it.documents.count())
+        }
     }
 }
