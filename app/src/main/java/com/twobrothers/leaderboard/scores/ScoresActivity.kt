ae8d54d4d4ff86d3705a84e9f523d04b7a552497
bigfriedicecream <michaeldsouza.md@gmail.com>
2019-12-08 10:45:08 +1100

    created toolbar with add icon

--- app/src/main/java/com/twobrothers/leaderboard/scores/ScoresActivity.kt
+++ app/src/main/java/com/twobrothers/leaderboard/scores/ScoresActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.twobrothers.leaderboard.R
@@ -26,10 +28,14 @@ class ScoresActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ScoresViewModel
     private lateinit var scoresListAdapter: ScoresListAdapter
+    private lateinit var addAction: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scores)
+
+        // Init Action Bar
+        setSupportActionBar(toolbar_action_bar)
 
         // Init view model
         val gameId = this.intent.getStringExtra(EXTRA_GAME_ID) ?: ""
@@ -50,6 +56,25 @@ class ScoresActivity : AppCompatActivity() {
         viewModel.scores.observe(this, Observer {
             scoresListAdapter.submitList(it)
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_add_score, menu)
+        addAction = menu.findItem(R.id.action_add)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_add -> {
+                // viewModel.onAddScoreClick()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
