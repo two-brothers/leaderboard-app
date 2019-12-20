a0e1b970d8c4fdee02760708deaf051b40692efd
nikeshnazareth <business@nikeshnazareth.com>
2019-12-20 21:27:01 +1100

    Create player model

---
+++ lib/models/player_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:equatable/equatable.dart';
+
+class PlayerModel extends Equatable {
+  final String id;
+  final String name;
+
+  PlayerModel({@required this.id, @required this.name});
+
+  factory PlayerModel.fromJson(String id, Map<String, dynamic> parsedJson) {
+    return PlayerModel(
+        id: id,
+        name: parsedJson['name']);
+  }
+
+  // Two objects with the same id will be assumed to be identical
+  @override
+  List<Object> get props => [id];
+}
