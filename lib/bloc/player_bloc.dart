a7c188e52416d6cf6b244ccfd8aace925e8d6c27
nikeshnazareth <business@nikeshnazareth.com>
2019-12-20 21:32:26 +1100

    Create PlayerBloc

---
+++ lib/bloc/player_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../models/player_model.dart';
+import 'bloc.dart';
+
+class PlayerBloc implements Bloc {
+  final Map<DocumentReference, BehaviorSubject<PlayerModel>> _players;
+
+  PlayerBloc() : _players = {};
+
+  Stream<PlayerModel> getPlayerByRef(DocumentReference playerRef) {
+    if (_players[playerRef] == null) {
+      _players[playerRef] = BehaviorSubject<PlayerModel>();
+      playerRef
+          .snapshots()
+          .map((document) => PlayerModel.fromJson(document.documentID, document.data))
+          .listen((player) => _players[playerRef].add(player));
+    }
+    return _players[playerRef];
+  }
+
+  @override
+  void dispose() {
+    _players.forEach((_, subject) => subject.close());
+  }
+}
