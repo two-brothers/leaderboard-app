c76de00c68418ab0e3c299189e2b6ded8e1bdf1b
nikeshnazareth <business@nikeshnazareth.com>
2019-12-26 19:01:57 +1100

    Update Game widget when leaderboard changes

--- lib/bloc/game_bloc.dart
+++ lib/bloc/game_bloc.dart
@@ -7,9 +7,14 @@ import 'bloc.dart';
 
 class GameBloc implements Bloc {
   final Map<SportsModel, BehaviorSubject<List<GameModel>>> _gamesBySport;
+  final Map<String, BehaviorSubject<GameModel>> _gamesById;
 
-  GameBloc() : _gamesBySport = {};
+  GameBloc()
+      : _gamesBySport = {},
+        _gamesById = {};
 
+  // Return a stream of games that match the specified sport.
+  // The game references are expanded so the titles can be displayed
   Stream<List<GameModel>> getGamesIn(SportsModel sport) {
     if (_gamesBySport[sport] == null) {
       _gamesBySport[sport] = BehaviorSubject<List<GameModel>>();
@@ -23,6 +28,22 @@ class GameBloc implements Bloc {
           .listen((games) => _gamesBySport[sport].add(games));
     }
     return _gamesBySport[sport];
+  }
+
+  // Return a stream of the specified game
+  // This stream will usually be contained within the getGamesIn stream for the relevant sport
+  // but we require a separate stream to track when the leaderboard changes
+  Stream<GameModel> getGame(String id) {
+    if (_gamesById[id] == null) {
+      _gamesById[id] = BehaviorSubject<GameModel>();
+      Firestore.instance
+          .collection('games')
+          .document(id)
+          .snapshots()
+          .map((document) => GameModel.fromJson(document.documentID, document.data))
+          .listen((game) => _gamesById[id].add(game));
+    }
+    return _gamesById[id];
   }
 
   @override
--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'bloc/bloc_provider.dart';
+import 'bloc/game_bloc.dart';
 import 'bloc/player_bloc.dart';
 import 'models/game_model.dart';
 import 'models/player_model.dart';
@@ -13,30 +14,35 @@ class Game extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final PlayerBloc _bloc = BlocProvider.of<PlayerBloc>(context);
+    final PlayerBloc _playerBloc = BlocProvider.of<PlayerBloc>(context);
+    final GameBloc _gameBloc = BlocProvider.of<GameBloc>(context);
 
-    return Scaffold(
-      appBar: AppBar(title: Text(game.title)),
-      body: Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: <Widget>[
-              Text(game.summary, style: Theme.of(context).textTheme.title),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: game.leaderboard.length,
-                    itemBuilder: (context, index) => StreamBuilder<PlayerModel>(
-                        stream: _bloc.getPlayerStreamByRef(game.leaderboard[index].playerRef),
-                        builder: (context, snapshot) => ListTile(
-                            title: Text(
-                                '${index + 1}. ${snapshot.hasData ? game.leaderboard[index].display(snapshot.data) : '...'}')))),
+    return StreamBuilder<GameModel>(
+        stream: _gameBloc.getGame(game.id),
+        builder: (context, gameSnapshot) => gameSnapshot.hasData
+            ? Scaffold(
+                appBar: AppBar(title: Text(gameSnapshot.data.title)),
+                body: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      children: <Widget>[
+                        Text(gameSnapshot.data.summary, style: Theme.of(context).textTheme.title),
+                        Expanded(
+                          child: ListView.builder(
+                              itemCount: gameSnapshot.data.leaderboard.length,
+                              itemBuilder: (context, index) => StreamBuilder<PlayerModel>(
+                                  stream: _playerBloc.getPlayerStreamByRef(gameSnapshot.data.leaderboard[index].playerRef),
+                                  builder: (context, playerSnapshot) => ListTile(
+                                      title: Text(
+                                          '${index + 1}. ${playerSnapshot.hasData ? gameSnapshot.data.leaderboard[index].display(playerSnapshot.data) : '...'}')))),
+                        )
+                      ],
+                    )),
+                floatingActionButton: FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () =>
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => NewMatch(game: gameSnapshot.data)))),
               )
-            ],
-          )),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => NewMatch(game: game)))
-      ),
-    );
+            : Center(child: CircularProgressIndicator()));
   }
 }
