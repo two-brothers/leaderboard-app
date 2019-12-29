121c915ab672bf023026fa5bcf224bb7ababc97f
nikeshnazareth <business@nikeshnazareth.com>
2019-12-29 16:19:01 +1100

    create UI for ranked matches

--- lib/bloc/match_bloc.dart
+++ lib/bloc/match_bloc.dart
@@ -6,12 +6,18 @@ import '../models/player_model.dart';
 import 'bloc.dart';
 
 class MatchBloc implements Bloc {
-  Future<DocumentReference> addPersonalScore(
-          {@required DateTime dt, @required GameModel game, @required PlayerModel player, @required int score}) =>
+  Future<DocumentReference> addPersonalScore({@required DateTime dt, @required GameModel game, @required PlayerModel player, @required int score}) =>
       Firestore.instance.collection('matches').add({
         'date': dt,
         'game': Firestore.instance.collection('games').document(game.id),
         'result': {'player': Firestore.instance.collection('players').document(player.id), 'score': score}
+      });
+
+  Future<DocumentReference> addRankedMatch({@required DateTime dt, @required GameModel game, @required PlayerModel winner, @required PlayerModel loser}) =>
+      Firestore.instance.collection('matches').add({
+        'date': dt,
+        'game': Firestore.instance.collection('games').document(game.id),
+        'result': {'winner': Firestore.instance.collection('players').document(winner.id), 'loser': Firestore.instance.collection('players').document(loser.id)}
       });
 
   @override
--- lib/new_match.dart
+++ lib/new_match.dart
@@ -25,6 +25,8 @@ class NewMatch extends StatefulWidget {
         return _NewPersonalScoreState();
       case GameType.RANKING:
         return _RankScoreState();
+      default:
+        return null;
     }
   }
 }
@@ -38,60 +40,70 @@ abstract class _NewMatchState extends State<NewMatch> {
     return Scaffold(
         appBar: AppBar(title: Text('Record Match')),
         body: Builder(
-          builder: (BuildContext context) => SingleChildScrollView(
-            child: Container(
-                padding: EdgeInsets.all(16),
-                child: Form(
-                    key: this.widget._formKey,
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Text(this.widget.game.title, style: Theme.of(context).textTheme.title),
-                      ),
-                      DateTimeField(
-                          format: this.widget._format,
-                          initialValue: _dt,
-                          decoration: InputDecoration(hintText: 'Date of match'),
-                          validator: (value) => value == null ? 'Enter date of match' : null,
-                          onShowPicker: (context, currentValue) => showDatePicker(
-                              context: context,
-                              firstDate: DateTime(1900),
-                              initialDate: currentValue ?? DateTime.now(),
-                              lastDate: DateTime(2100)),
-                          onSaved: (value) => setState(() => _dt = value),
-                          readOnly: true),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: formElements(context),
-                      ),
-                      Center(
-                          child: Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: RaisedButton(
-                                  onPressed: () {
-                                    if (this.widget._formKey.currentState.validate()) {
-                                      this.widget._formKey.currentState.save();
-                                      setState(() => _saving = true);
-                                      save(context).then((_) {
-                                        setState(() => _saving = false);
-                                        Scaffold.of(context).showSnackBar(
-                                            SnackBar(content: Text('Saved Record'), backgroundColor: Colors.green));
-                                        this.widget._formKey.currentState.reset();
-                                      }).catchError((error) {
-                                        setState(() => _saving = false);
-                                        Scaffold.of(context).showSnackBar(
-                                            SnackBar(content: Text(error.toString()), backgroundColor: Colors.red));
-                                        this.widget._formKey.currentState.reset();
-                                      });
-                                    }
-                                  },
-                                  child: Text('RECORD', style: Theme.of(context).textTheme.button),
-                                  color: Theme.of(context).primaryColor))),
-                      ConditionalBuilder(
-                          condition: _saving,
-                          builder: (context) => Expanded(child: Center(child: CircularProgressIndicator())))
-                    ]))),
-          ),
+          builder: (BuildContext context) =>
+              SingleChildScrollView(
+                child: Container(
+                    padding: EdgeInsets.all(16),
+                    child: Form(
+                        key: this.widget._formKey,
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Text(this.widget.game.title, style: Theme
+                                .of(context)
+                                .textTheme
+                                .title),
+                          ),
+                          DateTimeField(
+                              format: this.widget._format,
+                              initialValue: _dt,
+                              decoration: InputDecoration(hintText: 'Date of match'),
+                              validator: (value) => value == null ? 'Enter date of match' : null,
+                              onShowPicker: (context, currentValue) =>
+                                  showDatePicker(
+                                      context: context,
+                                      firstDate: DateTime(1900),
+                                      initialDate: currentValue ?? DateTime.now(),
+                                      lastDate: DateTime(2100)),
+                              onSaved: (value) => setState(() => _dt = value),
+                              readOnly: true),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: formElements(context),
+                          ),
+                          Center(
+                              child: Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: RaisedButton(
+                                      onPressed: () {
+                                        if (this.widget._formKey.currentState.validate()) {
+                                          this.widget._formKey.currentState.save();
+                                          setState(() => _saving = true);
+                                          save(context).then((_) {
+                                            setState(() => _saving = false);
+                                            Scaffold.of(context).showSnackBar(
+                                                SnackBar(content: Text('Saved Record'), backgroundColor: Colors.green));
+                                            this.widget._formKey.currentState.reset();
+                                          }).catchError((error) {
+                                            setState(() => _saving = false);
+                                            Scaffold.of(context).showSnackBar(
+                                                SnackBar(content: Text(error.toString()), backgroundColor: Colors.red));
+                                            this.widget._formKey.currentState.reset();
+                                          });
+                                        }
+                                      },
+                                      child: Text('RECORD', style: Theme
+                                          .of(context)
+                                          .textTheme
+                                          .button),
+                                      color: Theme
+                                          .of(context)
+                                          .primaryColor))),
+                          ConditionalBuilder(
+                              condition: _saving,
+                              builder: (context) => Expanded(child: Center(child: CircularProgressIndicator())))
+                        ]))),
+              ),
         ));
   }
 
@@ -105,9 +117,10 @@ class _NewPersonalScoreState extends _NewMatchState {
   int _score;
 
   @override
-  List<Widget> formElements(BuildContext context) => [
+  List<Widget> formElements(BuildContext context) =>
+      [
         PlayerSelectorFormField(
-            hintText: 'Player name',
+            hintText: 'Player',
             validator: (value) => value == null ? 'Choose player' : null,
             onSaved: (value) => setState(() => _player = value)),
         TextFormField(
@@ -118,16 +131,33 @@ class _NewPersonalScoreState extends _NewMatchState {
             onSaved: (value) => setState(() => _score = int.parse(value)))
       ];
 
-  Future save(context) {
+  @override
+  Future save(BuildContext context) {
     final MatchBloc _bloc = BlocProvider.of<MatchBloc>(context);
     return _bloc.addPersonalScore(dt: _dt, game: this.widget.game, player: _player, score: _score);
   }
 }
 
 class _RankScoreState extends _NewMatchState {
-  @override
-  List<Widget> formElements(BuildContext context) => [];
+  PlayerModel _winner;
+  PlayerModel _loser;
 
   @override
-  Future save(BuildContext context) => Future.error('Not implemented');
+  List<Widget> formElements(BuildContext context) =>
+      [
+        PlayerSelectorFormField(
+            hintText: 'Winner',
+            validator: (value) => value == null ? 'Choose winner' : null,
+            onSaved: (value) => setState(() => _winner = value)),
+        PlayerSelectorFormField(
+            hintText: 'Loser',
+            validator: (value) => value == null ? 'Choose loser' : null,
+            onSaved: (value) => setState(() => _loser = value))
+      ];
+
+  @override
+  Future save(BuildContext context) {
+    final MatchBloc _bloc = BlocProvider.of<MatchBloc>(context);
+    return _bloc.addRankedMatch(dt: _dt, game: this.widget.game, winner: _winner, loser: _loser);
+  }
 }
