7592ec47f0d527d4bd439180707351620265e5e9
nikeshnazareth <business@nikeshnazareth.com>
2020-01-19 13:20:16 +1100

    Add pseudorandom color to PlayerAvatar

--- lib/player_avatar.dart
+++ lib/player_avatar.dart
@@ -1,13 +1,29 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'models/player_model.dart';
 
-class PlayerAvatar extends CircleAvatar {
+class PlayerAvatar extends StatelessWidget {
   final PlayerModel player;
+  final double radius;
 
-  PlayerAvatar({@required this.player, double radius})
-      : super(
-            backgroundImage: player.avatarUrl == null ? null : NetworkImage(player.avatarUrl),
-            child: player.avatarUrl == null ? Text(player.name.substring(0, 1)) : null,
-            radius: radius);
+  PlayerAvatar({@required this.player, this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+        backgroundImage: player.avatarUrl == null ? null : NetworkImage(player.avatarUrl),
+        backgroundColor: pseudorandomColor(player.id),
+        child: player.avatarUrl == null
+            ? Text(player.name.substring(0, 1), style: TextStyle(backgroundColor: pseudorandomColor(player.id)))
+            : null,
+        radius: radius);
+  }
+
+  Color pseudorandomColor(String input) {
+    // note: this method will have color collisions
+    final Random rng = Random(input.codeUnits.reduce((a, b) => a + b));
+    return Color.fromARGB(0xff, rng.nextInt(0xff), rng.nextInt(0xff), rng.nextInt(0xff));
+  }
 }
