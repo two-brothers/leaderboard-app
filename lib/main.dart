2e71b77b368e2597c60f91563773b5c57c534562
bigfriedicecream <michaeldsouza.md@gmail.com>
2020-11-26 21:39:22 +1100

    Style statusbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:wakelock/wakelock.dart';
 
@@ -8,6 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Wakelock.enable();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.black));
     return MaterialApp(
       title: 'Leaderboard',
       routes: {
