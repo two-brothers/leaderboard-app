f5d0d4e9c3c2b181a9c9881bb0315c37e3319a1b
bigfriedicecream <michaeldsouza.md@gmail.com>
2020-11-26 21:19:03 +1100

    Add webview widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:wakelock/wakelock.dart';
 
-import 'bloc/bloc_provider.dart';
-import 'bloc/game_bloc.dart';
-import 'bloc/match_bloc.dart';
-import 'bloc/player_bloc.dart';
-import 'bloc/sports_bloc.dart';
-import 'home.dart';
+void main() => runApp(App());
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Wakelock.enable();
-    return _provideBlocs(MaterialApp(
+    return MaterialApp(
       title: 'Leaderboard',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: TextTheme(
-            button: TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.white),
-          )),
-      home: Home(),
-    ));
-  }
-
-  Widget _provideBlocs(Widget child) {
-    return BlocProvider<SportsBloc>(
-        bloc: SportsBloc(),
-        child: BlocProvider<GameBloc>(
-            bloc: GameBloc(),
-            child: BlocProvider<PlayerBloc>(
-                bloc: PlayerBloc(), child: BlocProvider<MatchBloc>(bloc: MatchBloc(), child: child))));
+      routes: {
+        "/": (_) => WebviewScaffold(
+              url: "https://leaderboard-19f69.web.app/leaderboards",
+            )
+      },
+    );
   }
 }
--- lib/models/score_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
-
-class Score {
-  final DocumentReference playerRef;
-  final int score;
-
-  Score({@required this.playerRef, @required this.score});
-}
